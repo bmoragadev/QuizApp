d986cb0dcddfce2d72fe0c3bc59d2f8015462cb9
Benjamin Moraga Rodriguez <bean.moro@gmail.com>
2026-03-11 21:02:04 -0300

    Se agregaron fragments y se conectaron en la navegacion

---
+++ app/src/main/java/com/example/quizapp/InitialFragment.kt
@@ -0,0 +1,30 @@
+package com.example.quizapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.quizapp.databinding.FragmentInitialBinding
+
+
+class InitialFragment : Fragment() {
+
+    private var _binding : FragmentInitialBinding ? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentInitialBinding.inflate(inflater, container, false)
+
+
+
+        return binding.root
+    }
+
+
+}
